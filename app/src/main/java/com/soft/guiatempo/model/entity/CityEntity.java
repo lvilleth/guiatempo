1fc2f4adefbeb5853c4c04605c658790b8f5d410
Leonardo Villeth <lvilleth@gmail.com>
2020-02-06 18:11:52 -0300

    Initial Commit

---
+++ app/src/main/java/com/soft/guiatempo/model/entity/CityEntity.java
@@ -0,0 +1,36 @@
+package com.soft.guiatempo.model.entity;
+
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "city")
+public class CityEntity {
+
+    @PrimaryKey(autoGenerate = true)
+    @NonNull
+    private Long id;
+
+    private String name;
+
+    @NonNull
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(@NonNull Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public CityEntity(String name){
+        setName(name);
+    }
+}
